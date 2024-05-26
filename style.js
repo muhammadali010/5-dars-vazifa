a7d19187f799afaa2cee54960e7c921cc77d985b
muhammadali010 <muhammadjonovmuhammadali32@gmail.com>
2024-05-27 03:22:21 +0500

    tugadi

---
+++ style.js
@@ -0,0 +1,144 @@
+// 1chi masala
+// let n = + prompt("son kiriting");
+// for(let i = 1; i<=n; i++ ){
+// console.log(i);
+// }
+
+
+// 2chi masala
+// let n = + prompt("son kiriting");
+// for(let i = n; i<=1; i++ ){
+// console.log(i);
+// }
+
+
+// 3chi masala
+// let a=0;
+// let n = + prompt("son kiriting");
+// for(let i =1; i<= n; i++) {
+// a = a + i;
+// }
+// console.log(a)
+
+
+// 4chi masala
+// let a=0;
+// let n = + prompt("son kiriting");
+// for(let i =1; i<= n; i++) {
+//     if(i%2==0){
+//         a=a+i;
+// }
+// }
+// console.log(a);
+
+
+// 5chi masala
+// let a=0;
+// let n = + prompt("son kiriting");
+// for(let i =1; i<= n; i++) {
+//     if(i%2==1){
+//         a=a+i;
+// }
+// }
+// console.log(a);
+
+
+// 6chi masala
+// let n = + prompt("son kiriting");
+// for (let i = 1; i <= n; i++) {
+//     if (n%i==0){
+//     console.log(i);
+//     }
+// }
+
+
+// 7chi masala
+// let n = + prompt("son kitriting");
+// let count=0
+// for (let i = 1; i <= n; i++) {
+//     if (n%i==0){
+//         count++
+//         console.log(count);
+//     }
+// }
+
+
+// 8chi masala
+// let n = + prompt("son kiriting");
+// let count = 0;
+// for (let i = 1; i <= n; i++) {
+//     if (n%i==0){
+// count++
+//     }
+// }
+// if(count == 2){
+// console.log("tub son");
+// }else{
+// console.log("tub son emas");
+// }
+
+
+// for6
+// let a = + prompt("son kiriting");
+// for (let i=1; i<=1; i+0.2){
+// console.log(a*i);
+// }
+
+
+// for7
+// let a = + prompt("son kiriting");
+// let b = + prompt("son kiriting");
+// let c=0;
+// for(let i =a; i<= b; i++) {
+// c = c + i;
+// }
+// console.log(c);
+
+
+// for8
+// let a = + prompt("son kirirting");
+// let b = + prompt("son kiriting");
+// let c=1;
+// for(let i =a; i<= b; i++) {
+// c = c * i;
+// }
+// console.log(c);
+
+
+// for9
+// let a = + prompt("son kiriting");
+// let b = + prompt("son kiriting");
+// let c=1;
+// for(let i =a; i<= b; i++) {
+// c = c * i*i;
+// }
+// console.log(c);
+
+
+// for10
+// let a = + prompt("son kiriting");
+// let b = 0
+// for(let i =1; i<=a; i++){
+// b=b+1/i;
+// }
+// console.log(b);
+
+
+// for11
+// let a = + prompt("son kiriting");
+// let b = 0;
+// for(let i =0; i<=a; i++){
+// b=b+Math.pow((a+i),2);
+// }
+// console.log(b);
+
+
+// for12
+// let a= + prompt("son kiriting");
+// let b=1;
+// let c=1.1;
+// for(let i = 1; i<=a; i++){
+// b=b*c;
+// c=c+0.1;
+// }
+// console.log(b);
